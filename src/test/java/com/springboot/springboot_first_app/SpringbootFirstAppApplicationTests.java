8df9db028909f271c8a2888f3f5e68b2325b0e7c
Rahul TS <tsrahul1996@gmail.com>
2026-01-09 15:30:43 +0000

    first commit - adding springboot starter project

---
+++ src/test/java/com/springboot/springboot_first_app/SpringbootFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springboot_first_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
