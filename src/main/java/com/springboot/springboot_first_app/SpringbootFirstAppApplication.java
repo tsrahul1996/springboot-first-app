16cfe9ba815c5b6dfc6e94abc748c8c5ce79925d
Rahul TS <tsrahul1996@gmail.com>
2026-01-10 15:19:06 +0000

    added spring boot starter jpa disabled auto config for data source,jdbc and jdbc templates

--- src/main/java/com/springboot/springboot_first_app/SpringbootFirstAppApplication.java
+++ src/main/java/com/springboot/springboot_first_app/SpringbootFirstAppApplication.java
@@ -2,8 +2,17 @@ package com.springboot.springboot_first_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.JdbcClientAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.JdbcTemplateAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+
+		DataSourceAutoConfiguration.class,
+		JdbcClientAutoConfiguration.class,
+		JdbcTemplateAutoConfiguration.class
+
+})
 public class SpringbootFirstAppApplication {
 
 	public static void main(String[] args) {
