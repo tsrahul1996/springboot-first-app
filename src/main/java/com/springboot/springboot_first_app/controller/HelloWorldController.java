8506e18e5da58fb1dffe181112584c8f7e0b1f99
Rahul TS <tsrahul1996@gmail.com>
2026-01-10 14:09:09 +0000

    added new controller to test hello world

---
+++ src/main/java/com/springboot/springboot_first_app/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.springboot.springboot_first_app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+
+        return "Hello world!";
+    }
+
+}
